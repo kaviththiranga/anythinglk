4c99779380e97aff2d2d50f32f819a597b03e103
Kavith Thiranga Lokuhewage <rc404mekt@gmail.com>
2013-05-06 12:01:24 +0530

    added order controller

---
+++ App_Code/Controllers/OrderController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for OrderController
+/// </summary>
+public class OrderController
+{
+	public OrderController()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}
